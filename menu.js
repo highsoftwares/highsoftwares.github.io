9c7b12dbf9bec03de54de594d07d2327bb8a7b54
Gabriel <worpressmail.mail@gmail.com>
2018-01-21 22:20:02 -0200

    Initial commit

---
+++ menu.js
@@ -0,0 +1,18 @@
+function openNav()
+{
+    document.getElementById('mySideNav').style.width = "350px";
+
+}
+function closeNav()
+{
+    document.getElementById('mySideNav').style.width = "0px";
+}
+// Felipe
+function InfoFelipe ()
+{
+  document.getElementById('SideNavTM').style.width = "350px";
+}
+function menosFelipe ()
+{
+  document.getElementById('SideNavTM').style.width = "0px";
+}
